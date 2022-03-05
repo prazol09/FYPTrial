f1ede0170df8306b41e347e5ba3dce1ff95f4e4f
prazol09 <prazolbasnet11@gmail.com>
2022-03-05 11:04:22 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# this file contains a function that takes an image as input and recognize
+# weather it is a hand or not
+
+#def hand_reco(image):
+    #code to
+
+
+#image = open('ladyBo.png')
+#load('ladyBo.png')
+from PIL import Image    
+image = Image.open("ladyBo.png")
+image.show()
